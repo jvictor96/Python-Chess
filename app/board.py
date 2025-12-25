aa3982fb2984afc80799928e30f2605a72232766
José Victor <jvictor96@gmail.com>
2025-12-25 19:22:57 -0300

    daemon

--- app/board.py
+++ app/board.py
@@ -1,9 +1,53 @@
-from abc import abstractmethod
 import json
+import time
 from movement import Movement
 from piece import Piece, PieceSerializer
 from position import Position
 
+class ChessDaemon:
+    def main_loop(self):
+        while True:
+            time.sleep(0.1)
+            with open("app/daemon.txt", "r+") as daemon:
+                daemon.seek(0)
+                daemon.truncate()
+                control_fields = json.load(daemon)
+                for game in control_fields["games"]:
+                    self.update_game(game)
+                if control_fields["new_game"]:
+                    control_fields["new_game"] = False
+                    control_fields["games"].append(control_fields["next_id"])
+                    BoardIO.save_board(
+                        board = BoardIO.get_board(0),
+                        game_id=control_fields["next_id"])
+                    self.reprint_input_output(
+                        game_id=control_fields["next_id"],
+                        board=BoardIO.get_board(0))
+                    control_fields["next_id"] += 1
+                if control_fields["end_game"] > 0:
+                    control_fields["games"].remove(control_fields["end_game"])
+                    control_fields["new_game"] = 0
+                json.dump(control_fields, daemon)
+
+    def update_game(self, game: int):
+        with open(f"app/games/game_{game}_input.txt", "r+") as game_file:
+            game_file.seek(0)
+            game_file.truncate()
+            game_control_fields = json.load(game_file)
+            if game_control_fields["move"] != "":
+                board = BoardIO.get_board(game)
+                board = board.move(game_control_fields["move"])
+                BoardIO.save_board(board, game)
+                self.reprint_input_output(game, board, "" if board.legal else "Illegal move")
+
+    def reprint_input_output(self, game_id: int, board: "Board", error: str = ""):
+        with open(f"app/games/game_{game_id}_output.txt", "w") as output_file:
+            [output_file.write(line + "\n") for line in BoardIO.display(board)]
+        game_control_fields = json.loads("{}")
+        game_control_fields["move"] = ""
+        game_control_fields["error"] = error
+        with open(f"app/games/game_{game_id}_input.txt", "w") as input_file:
+            [input_file.write(line + "\n") for line in BoardIO.display(board)]
 
 class BoardIO:
 
@@ -31,11 +75,12 @@ class BoardIO:
                 [PieceSerializer.deserialize(piece) for piece in game["pieces"]], game["movements"])
         return board
 
-    def save_board(self, game_id: int):
+    @staticmethod
+    def save_board(board: "Board", game_id: int):
         with open(f"app/games/game_{game_id}.txt", "w") as file:
             game = {
-                "pieces": [PieceSerializer.serialize(piece) for piece in self.pieces],
-                "movements": self.movements
+                "pieces": [PieceSerializer.serialize(piece) for piece in board.pieces],
+                "movements": board.movements
             }
             json.dump(game, file)
 
