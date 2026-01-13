72727ff6d8a5a053d49e10ee22ac34b760a07ac2
José Victor <jvictor96@gmail.com>
2026-01-13 18:31:26 -0300

    test checkmate

--- core/tests/test_check_movement.py
+++ core/tests/test_check_movement.py
@@ -5,11 +5,23 @@ from piece import King, Queen, Pawn
 # game id 3 had d2 and e2 free and black bishop at b4
 
 @pytest.fixture
+def pastor():
+        board = Board()
+        board.bypass_validation_move("e2e4")  
+        board.bypass_validation_move("f1c4")  
+        board.bypass_validation_move("d1f3")  
+        board.white = "jose"
+        board.black = "mike"
+        return board
+
+@pytest.fixture
 def king_in_check_by_bishop():
         board = Board()
         board.bypass_validation_move("d2d4")  
         board.bypass_validation_move("e2e4")  
         board.bypass_validation_move("f8b4")  
+        board.white = "jose"
+        board.black = "mike"
         return board
 
 # game id 4 had black bishop at b4
@@ -18,6 +30,8 @@ def king_in_check_by_bishop():
 def board4():
         board = Board()
         board.bypass_validation_move("f8b4")  
+        board.white = "jose"
+        board.black = "mike"
         return board
 
 # game id 5 e2 free and black bishop at g4
@@ -27,37 +41,47 @@ def board5():
         board = Board()
         board.bypass_validation_move("e2e4")  
         board.bypass_validation_move("c8g4")  
+        board.white = "jose"
+        board.black = "mike"
         return board
 
 def test_king_move_out_check(king_in_check_by_bishop: Board):
     king_in_check_by_bishop.move("e1e2")
     assert king_in_check_by_bishop.legal == True
     assert isinstance(king_in_check_by_bishop.positions.get("e2", None), King)
+    assert king_in_check_by_bishop.winner == ""
 
 def test_king_move_is_still_check(king_in_check_by_bishop: Board):
     king_in_check_by_bishop.move("e1d2")
     assert king_in_check_by_bishop.legal == False
     assert isinstance(king_in_check_by_bishop.positions.get("e1", None), King)
+    assert king_in_check_by_bishop.winner == ""
 
 def test_check_is_blocked_by_queen(king_in_check_by_bishop: Board):
     king_in_check_by_bishop.move("d1d2")
     assert king_in_check_by_bishop.legal == True
     assert isinstance(king_in_check_by_bishop.positions.get("d2", None), Queen)
+    assert king_in_check_by_bishop.winner == ""
 
 def test_queen_moves_but_still_check(king_in_check_by_bishop: Board):
     king_in_check_by_bishop.move("d1d3")
     assert king_in_check_by_bishop.legal == False
     assert isinstance(king_in_check_by_bishop.positions.get("d1", None), Queen)
+    assert king_in_check_by_bishop.winner == ""
 
 def test_invalid_pawn_moves_and_puts_king_in_check(board4: Board):
     board4.move("d2d3")
     assert board4.legal == False
     assert isinstance(board4.positions.get("d2", None), Pawn)
+    assert board4.winner == ""
 
 def test_invalid_king_goes_to_check(board5: Board):
     board5.move("e1e2")
     assert board5.legal == False
     assert isinstance(board5.positions.get("e1", None), King)
+    assert board5.winner == ""
 
-def test_check_mate(board5: Board):
-    assert True == False
+def test_check_mate(pastor: Board):
+    pastor.move("f3f7")
+    assert pastor.legal == True
+    assert pastor.winner == "jose"
